0ff092d0014ce113881396aa4164c8b6bc2475c0
ekw544 <elisawood10@aol.com>
2018-02-04 15:02:01 -0600

    Added results action, displaying search results, displays list of all jobs

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -16,5 +16,31 @@ namespace TechJobs.Controllers
         // TODO #1 - Create a Results action method to process 
         // search request and display results
 
+        [HttpGet]
+        [Route("/Search/Results")]
+        public IActionResult SearchResults(string searchType, string searchTerm)
+        {
+            //should take in 2 parameters, specifying the type of search and 
+            //the search term. Need to name params appropriately based on form
+            //field names
+            ViewBag.columns = ListController.columnChoices;
+
+            if (searchTerm == null)
+            {
+                ViewBag.jobs = JobData.FindAll();
+            }
+
+            else if (searchType == "all")
+            {
+                ViewBag.jobs = JobData.FindByValue(searchTerm);
+            }
+
+            else
+            {
+                ViewBag.jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            }
+
+            return View("Views/Search/Index.cshtml");
+        }
     }
 }
